3c835584d8fb1d5301a760d80a4290ec86db6c0b
Tim Lucas <t.lucas@toolmantim.com>
2013-05-12 11:45:30 +1000

    Got a layout goin on

--- Commands/Start.command
+++ Commands/Start.command
@@ -1,6 +1,7 @@
 cd `dirname $0`/..
 
-echo "Starting the Foxglove site (Ctrl-C to abort)..."
+echo
+echo "Starting the Foxglove site (Ctrl-C to stop)..."
 echo
 
-npm install && npm start
+npm install && node node_modules/supervisor/lib/cli-wrapper.js app.js
--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 var express = require('express'),
     http = require('http'),
-    path = require('path');
+    path = require('path'),
+    exphbs = require('express3-handlebars');
 
 var app = express();
 
-// all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
 app.use(express.compress());
 app.use(express.favicon());
 app.use(express.logger('dev'));
@@ -16,15 +18,14 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
 app.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('home', {title:'Foxglove Lettering'});
 });
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Foxglove site running on http://localhost:' + app.get('port') + '/');
+  console.log("Foxglove site running on http://localhost:" + app.get('port') + "/");
 });
